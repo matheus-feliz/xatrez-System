141caef0f72a4e0fc5c9124959e0f7ef71524086
matheus-feliz <matheusfoliveira172@gmail.com>
2021-05-04 16:59:38 -0300

    Knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -76,7 +76,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  a b v d e f g h");
+		System.out.println("  a b c d e f g h");
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves ) {
